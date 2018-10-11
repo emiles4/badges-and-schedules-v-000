8ecc204fdfa138d58a03f15dd16da3efeb1f76b0
Emily Miles <emiles4@gmail.com>
2018-10-11 22:48:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_bage_creator(attendees)
+  attendees.collect do |name|
+    badge_maker(name)
+  end
+end
+
+
+def assign_rooms
+room=0
+  attendees.collect do |name|
+      room+=1
+      "Hello, #{name}! You'll be assigned to room #{room}!"
+end
+end
+#def printer
+#end
