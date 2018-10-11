a8c2a7f658f06c8b34975942f5c531860d315899
Emily Miles <emiles4@gmail.com>
2018-10-11 23:10:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,3 +13,9 @@ new_arr << "Hello, #{name}! You'll be assigned to room #{i+1}!"
   end
   new_arr
 end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do {|badge|  puts badge}
+
+
+end
